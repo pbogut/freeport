c6dd428e89e40e7d649829b2fd8d3e3f3aaf5ebe
mgutz <32134+mgutz@users.noreply.github.com>
2021-06-29 05:11:54 +0800

    feat: add Min, Max and base Address options

--- freeport.go
+++ freeport.go
@@ -1,47 +1,103 @@
 package freeport
 
 import (
+	"errors"
+	"fmt"
 	"net"
+	"os"
+	"strconv"
 )
 
-// GetFreePort asks the kernel for a free open port that is ready to use.
-func GetFreePort() (int, error) {
-	addr, err := net.ResolveTCPAddr("tcp", ":0")
-	if err != nil {
-		return 0, err
+type Options struct {
+	Address string
+	Min     int
+	Max     int
+}
+
+// MakeOptions makes options to get non-privileged port [1024:49151]. It will use
+// env var PORT, if set, as min value.
+func MakeOptions(isAllInterfaces bool) (*Options, error) {
+	min := 0
+
+	s := os.Getenv("PORT")
+	if s != "" {
+		min, _ = strconv.Atoi(s)
 	}
 
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return 0, err
+	if min == 0 {
+		min = 1024
 	}
-	l.Close()
-	return l.Addr().(*net.TCPAddr).Port, nil
+
+	address := "127.0.0.1"
+	if isAllInterfaces {
+		address = ""
+	}
+
+	// limit to non-privileged ports, sys apps should not be using FreePort
+	return &Options{Address: address, Min: min, Max: 49151}, nil
+}
+
+var ErrPortNotFound = errors.New("port not found")
+
+// GetFreePortEx asks the kernel for a free open port that is ready to use. If options is nil, then
+// default options are used.
+func GetFreePortEx(options *Options) (int, error) {
+	var err error
+
+	if options == nil {
+		options, err = MakeOptions(false)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	for port := options.Min; port <= options.Max; port++ {
+		pingAddr := fmt.Sprintf("%s:%d", options.Address, port)
+		addr, err := net.ResolveTCPAddr("tcp", pingAddr)
+		if err != nil {
+			continue
+		}
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			continue
+		}
+
+		defer l.Close()
+		return l.Addr().(*net.TCPAddr).Port, nil
+	}
+
+	return 0, ErrPortNotFound
+}
+
+// GetFreePort gets non-privileged open port that is ready to use.
+func GetFreePort() (int, error) {
+	return GetFreePortEx(nil)
 }
 
 // MustGetFreePort calls GetFreePort and panics on error
 func MustGetFreePort() int {
-	port, err := GetFreePort()
+	port, err := GetFreePortEx(nil)
 	if err != nil {
 		panic(err)
 	}
 	return port
 }
 
-// GetFreePorts asks the kernel for free open ports that are ready to use.
+// GetFreePorts gets an array of non-privileged open ports that are ready to use.
 func GetFreePorts(count int) ([]int, error) {
 	ports := make([]int, count)
+	options, err := MakeOptions(false)
+	if err != nil {
+		return nil, err
+	}
+
 	for i := 0; i < count; i++ {
-		addr, err := net.ResolveTCPAddr("tcp", ":0")
-		if err != nil {
+		port, err := GetFreePortEx(options)
+		if err != nil && err != ErrPortNotFound {
 			return nil, err
 		}
-		l, err := net.ListenTCP("tcp", addr)
-		if err != nil {
-			return nil, err
-		}
-		defer l.Close()
-		ports[i] = l.Addr().(*net.TCPAddr).Port
+		ports[i] = port
+		options.Min = port + 1
 	}
 	return ports, nil
 }
--- freeport_test.go
+++ freeport_test.go
@@ -23,6 +23,33 @@ func TestGetFreePort(t *testing.T) {
 	defer l.Close()
 }
 
+func TestGetFreePortEx(t *testing.T) {
+	opts, err := MakeOptions(false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	opts.Min = 10000
+	port, err := GetFreePortEx(opts)
+	if err != nil {
+		t.Error(err)
+	}
+	if port == 0 {
+		t.Error("port == 0")
+	}
+
+	if port < opts.Min {
+		t.Error("expected port to be > 10000")
+	}
+
+	// Try to listen on the port
+	l, err := net.Listen("tcp", "localhost"+":"+strconv.Itoa(port))
+	if err != nil {
+		t.Error(err)
+	}
+	defer l.Close()
+}
+
 func TestGetFreePorts(t *testing.T) {
 	count := 3
 	ports, err := GetFreePorts(count)
